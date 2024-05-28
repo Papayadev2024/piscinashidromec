486aacc2836923cc46f2acab2aae8e4b0d26fd89
Jordan Daniel 27 <jordanmoreano.ppy@gmail.com>
2024-05-28 10:29:37 -0500

    update 1.0

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -53,7 +53,7 @@
         <h3 class="text-xs uppercase text-slate-500 font-semibold pl-3">
           <span class="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6"
             aria-hidden="true">•••</span>
-          <span class="lg:hidden lg:sidebar-expanded:block 2xl:block">Mundo Web - BackEnd</span>
+          <span class="lg:hidden lg:sidebar-expanded:block 2xl:block">Dr. Kewin - BackEnd</span>
         </h3>
         <ul class="mt-3">
 
