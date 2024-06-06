7c61c82c1b200f350e10c829fde5316fa7e6974a
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-06-06 15:41:27 -0500

    final

--- resources/views/pages/message/show.blade.php
+++ resources/views/pages/message/show.blade.php
@@ -3,39 +3,53 @@
         
         <div class="col-span-full xl:col-span-8 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
             <header class="px-5 py-4 border-b border-slate-100 dark:border-slate-700">
-                <h2 class="font-semibold text-slate-800 dark:text-slate-100 text-3xl">{{$message->full_name}} <span class="mx-4"><i class="fa-solid fa-phone-volume"></i></span>{{$message->phone}}</h2>
+                <h2 class="font-semibold text-slate-800 dark:text-slate-100 text-3xl">Mensaje de {{$message->full_name}}</h2>
             </header>
-            <div class="p-3">
+            <div class="">
         
                 <div class="px-6 pb-6">
 
-                    <div class="flex flex-col gap-3 pt-5 pb-10">
-                        <p>
-                           <span class="font-bold text-[18px]">Nombre:</span> <span class="text-[16px]">{{$message->full_name}}</span>
-                        </p>
-                        <p>
-                           <span class="font-bold text-[18px]">Teléfono:</span> <span class="text-[16px]">{{$message->phone}}</span>
-                        </p>
-                        <p>
-                           <span class="font-bold text-[18px]">Correo:</span> <span class="text-[16px]">{{$message->email}}</span>
-                        </p>
-
-                        <p>
-                            <span class="font-bold text-[18px]">Mensaje:</span> <span class="text-[16px]">{{$message->message}}</span>
-                         </p>
-
-                        {{-- <p>
-                            <span class="font-bold text-[18px]">{{$message->full_name}}</span>  esta interesado en tener informes del servicio: <span class="font-bold text-[18px]">{{$message->service_product}}</span> </p> --}}
-
-                    </div>
+                    <table class="w-full border-collapse my-8">
+                       
+                        <tbody>
+                            <tr>
+                                <td class="border px-4 py-2 font-bold text-[18px]">Nombre</td>
+                                <td class="border px-4 py-2 text-[16px]">{{$message->name}}</td>
+                            </tr>
+                            <tr>
+                                <td class="border px-4 py-2 font-bold text-[18px]">Apellidos</td>
+                                <td class="border px-4 py-2 text-[16px]">{{$message->last_name}}</td>
+                            </tr>
+                            <tr>
+                                <td class="border px-4 py-2 font-bold text-[18px]">Teléfono</td>
+                                <td class="border px-4 py-2 text-[16px]">{{$message->cellphone}}</td>
+                            </tr>
+                            <tr>
+                                <td class="border px-4 py-2 font-bold text-[18px]">Correo</td>
+                                <td class="border px-4 py-2 text-[16px]">{{$message->email}}</td>
+                            </tr>
+                            <tr>
+                                <td class="border px-4 py-2 font-bold text-[18px]">DNI</td>
+                                <td class="border px-4 py-2 text-[16px]">{{$message->document}}</td>
+                            </tr>
+                            <tr>
+                                <td class="border px-4 py-2 font-bold text-[18px]">Monto Solicitado</td>
+                                <td class="border px-4 py-2 text-[16px]">S/{{$message->monto}}.00</td>
+                            </tr>
+                            <tr>
+                                <td class="border px-4 py-2 font-bold text-[18px]">Dirección</td>
+                                <td class="border px-4 py-2 text-[16px]">{{$message->address}}</td>
+                            </tr>
+                            <tr>
+                                <td class="border px-4 py-2 font-bold text-[18px]">Garantia</td>
+                                <td class="border px-4 py-2 text-[16px]">{{$message->garantie}}</td>
+                            </tr>
+                        </tbody>
+                    </table>
                     
-                    {{-- <p><i class="fa-solid fa-at"></i> {{$message->email}}</p>
-                    
-                    <p class="my-10">
-                        {{$message->message}}
-                    </p> --}}
+                
 
-                    <a href="{{route('mensajes.index')}}" class="bg-blue-500 px-4 py-2 rounded text-white"><span><i class="fa-solid fa-arrow-left mr-2"></i></span> Volver</a>
+                    <a href="{{route('mensajes.index')}}" class=" bg-blue-500 px-4 py-2 rounded text-white"><span><i class="fa-solid fa-arrow-left mr-2"></i></span> Volver</a>
         
                 </div>
             </div>
