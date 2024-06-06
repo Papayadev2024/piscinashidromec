f65ee1d474292b7099def6529abe77e53565870e
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-06-06 13:15:35 -0500

    final

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -150,6 +150,7 @@ class IndexController extends Controller
                     rel="stylesheet"
                   />
                   <style>
+                    @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap");
                     * {
                       margin: 0;
                       padding: 0;
