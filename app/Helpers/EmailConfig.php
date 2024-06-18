7204ae311c1587099f4945dea2ba8a545cf367da
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-06-18 12:04:39 -0500

    final

--- app/Helpers/EmailConfig.php
+++ app/Helpers/EmailConfig.php
@@ -20,7 +20,7 @@ class EmailConfig
         $mail->Port = 465;
         $mail->Subject = '' . $name . ', ' .$mensaje;
         $mail->CharSet = 'UTF-8';
-        $mail->setFrom('diegomartinez1996x@gmail.com', 'Creditomype');
+        $mail->setFrom('diegomartinez1996x@gmail.com', 'Hidromec - Agrícola');
         return $mail;
     }
 }
