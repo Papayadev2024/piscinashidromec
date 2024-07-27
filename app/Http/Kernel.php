0b4f6c00fca39bcc1a00a59c3d3a6a0eca17ab0b
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-07-26 19:29:27 -0500

    final

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,7 +36,9 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Spatie\ResponseCache\Middlewares\CacheResponse::class
+            \DipeshSukhia\LaravelHtmlMinify\Middleware\LaravelMinifyHtml::class
+
+           
         ],
 
         'api' => [
@@ -64,7 +66,7 @@ class Kernel extends HttpKernel
         'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'doNotCacheResponse' => \Spatie\ResponseCache\Middlewares\DoNotCacheResponse::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class
+        
     ];
 }
